9bbacc2eeaa5d453fa7f1acd664b8c49445a6ff9
PabloFP64 <PabloFP64@gmail.com>
2022-10-07 15:48:51 +0200

    Añadido método de servicio.

--- src/main/java/demoapp/controller/NumData.java
+++ src/main/java/demoapp/controller/NumData.java
@@ -1,9 +1,15 @@
 package demoapp.controller;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 public class NumData {
+    @Min(value = -2147483648, message = "Age should not be less than -2147483648")
+    @Max(value = 2147483647, message = "Age should not be greater than 2147483647")
     int numeroInt;
 
     public int getNumeroInt() {
+
         return numeroInt;
     }
 
